c65234c3490fcb860ece24ae5f991141afe011b9
Taejin Kim <rlaxowls1316@naver.com>
2024-05-31 14:51:39 +0900

    feat: 홈 페이지 구현

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,39 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { HiHome } from "react-icons/hi";
+import { BiSolidBookmarks } from "react-icons/bi";
+import { FaUser } from "react-icons/fa";
 
 export default function Layout() {
   return (
     <div className="max-w-sm bg-[#F7F9FC] mx-auto min-h-svh">
       <Outlet />
+
+      <ul className=" text-[12px] max-w-sm mx-auto flex justify-between fixed bottom-0 left-0 right-0  p-6 bg-[#f8f8f8]">
+        <li>
+          <Link to="/">
+            <div className="text-[#EB4F30] flex flex-col items-center gap-1">
+              <HiHome size={18} />
+              <span>홈</span>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <div className="text-[#8F9BB3] flex flex-col items-center gap-1">
+              <BiSolidBookmarks size={18} />
+              <span>레시피</span>
+            </div>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            <div className="text-[#8F9BB3] flex flex-col items-center gap-1">
+              <FaUser size={18} />
+              <span>프로필</span>
+            </div>
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,43 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
       <div className="flex gap-5 p-4 content-center">
-        <div className="w-[30px] h-[30px] bg-red-100 rounded-full"></div>
+        <div className="w-[30px]  bg-red-100 rounded-full"></div>
         <h1
           style={{ color: "#EB3830", fontSize: "19px" }}
-          className=" ont-semibold"
+          className=" font-semibold"
         >
           레시피 가져오기
         </h1>
+      </div>
+
+      <div className="text-center h-[800px] flex flex-col gap-[30px] justify-center ">
+        <button>
+          <Link
+            to="/"
+            className="inline-block w-[181px] h-[119px] p-[40px] border-2 border-[#EB3830] rounded-[20px] bg-white"
+          >
+            Youtube로 가져오기
+          </Link>
+        </button>
+        <button>
+          <Link
+            to="/"
+            className=" inline-block w-[181px] h-[119px] p-[40px] border-2 border-[#EB3830] rounded-[20px] bg-white"
+          >
+            직접 메모하기
+          </Link>
+        </button>
+        <button>
+          <Link
+            to="/"
+            className=" inline-block w-[181px] h-[119px] p-[40px] border-2 border-[#EB3830] rounded-[20px] bg-white"
+          >
+            Website에서 가져오기
+          </Link>
+        </button>
       </div>
     </div>
   );
