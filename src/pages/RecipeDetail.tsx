965aef88bd36978e91217232028c5123a4e4f877
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-21 20:51:37 +0900

    레시피 페이지 헤더부분 스타일 변경

--- src/pages/RecipeDetail.tsx
+++ src/pages/RecipeDetail.tsx
@@ -7,17 +7,14 @@ export default function Recipe() {
   return (
     <div className="pb-10">
       <div className="relative">
-        <header className="text-[#ea4e30] font-bold  py-2 px-4 ">
-          <div className="flex items-center justify-between">
-            <div className="bg-white w-6 h-6 rounded-full flex items-center justify-center cursor-pointer">
-              <GrFormPrevious size={20} />
+        <div className="relative aspect-[1.4/1] bg-red-100">
+          <header className="absolute py-3 px-3">
+            <div className="flex items-center justify-between">
+              <div className="bg-white w-6 h-6 rounded-full flex items-center justify-center cursor-pointer">
+                <GrFormPrevious size={20} />
+              </div>
             </div>
-            <div className=" bg-white p-2 rounded-md text-[13px]">
-              맛있는 와타요업식 텐동 만들기
-            </div>
-          </div>
-        </header>
-        <div className="aspect-[1.4/1] bg-red-100">
+          </header>
           <img className="w-full h-full object-cover" src={Food} alt="food" />
         </div>
       </div>
