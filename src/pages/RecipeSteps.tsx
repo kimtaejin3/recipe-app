d182659c72d1cfb20e8896a1f9559f2a7e98cf84
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-23 20:30:12 +0900

    레시피 순서 페이지

--- src/pages/RecipeSteps.tsx
+++ src/pages/RecipeSteps.tsx
@@ -1,4 +1,5 @@
 import MikeIcon from "../assets/mike.svg";
+import stepCover from "../assets/stepCover.png";
 
 export default function RecipeSteps() {
   return (
@@ -18,6 +19,22 @@ export default function RecipeSteps() {
               </li>
             ))}
           </ul>
+        </div>
+
+        <div>
+          <p>미역은 물에 불려 준비한다.</p>
+          <div className="w-[300px] my-11 h-[190px] bg-red-50 mt-4 ">
+            <img className="w-full h-full object-cover" src={stepCover} />
+          </div>
+        </div>
+
+        <div className="mt-[50px] flex items-center justify-center gap-4">
+          <button className="border-[#EB4F30] border-2 border-solid rounded-[20px] py-2 px-[50px]">
+            이전단계
+          </button>
+          <button className="bg-[#EB4F30] rounded-[20px] py-2 px-[50px] text-white">
+            다음단계
+          </button>
         </div>
       </div>
     </>
