08761bbd89331586bf72a8ca0dc33263de3ecee3
kimtaejin3 <rlaxowls1316@naver.com>
2024-10-23 20:04:50 +0900

    재료 페이지

--- src/pages/RecipeIngredient.tsx
+++ src/pages/RecipeIngredient.tsx
@@ -50,7 +50,10 @@ export default function RecipeIngredient() {
           </li>
         </ul>
 
-        <div className="mt-[80px] flex items-center justify-center ">
+        <div className="mt-[50px] flex items-center justify-center gap-4">
+          <button className="border-[#EB4F30] border-2 border-solid rounded-[20px] py-2 px-[50px]">
+            이전단계
+          </button>
           <button className="bg-[#EB4F30] rounded-[20px] py-2 px-[50px] text-white">
             다음단계
           </button>
