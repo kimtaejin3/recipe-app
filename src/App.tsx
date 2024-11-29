74f6ce70d049a8341e89b538c707cd7604591b25
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-29 10:35:24 +0900

    유튜브로 가져오기 추가2

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import AddByHand from "./pages/AddByHand";
 import AddByWebsite from "./pages/AddByWebsite";
 import RecipeDetail from "./pages/RecipeDetail";
 import Recipes from "./pages/Recipes";
+import AddByYoutube from "./pages/AddByYoutube";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/controls" element={<Home />} />
           <Route path="/add-by-hand" element={<AddByHand />} />
           <Route path="/add-by-website" element={<AddByWebsite />} />
+          <Route path="/add-by-youtube" element={<AddByYoutube />} />
           <Route path="/recipe/:id" element={<RecipeDetail />} />
         </Route>
       </Routes>
